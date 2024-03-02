23f80a15e46a275177345f088492f7db8c0c6b1e
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-03-02 15:11:52 +1100

    bug fix

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -x
 
 apt-get upgrade -y && apt-get update -y && apt-get install unzip -y
-pip install --upgrade termcolor matplotlb opencv-python scikit-learn
+pip install --upgrade termcolor matplotlib opencv-python scikit-learn
 cd datasets
 wget -q --show-progress "https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz"
-unzip -qq CUB_200_2011.zip
+tar -zxf CUB_200_2011.tgz
